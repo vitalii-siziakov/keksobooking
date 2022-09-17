1b4642fcb81ae2544d8d1f1828cd25879c0e6828
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2022-09-17 12:53:15 +0000

    feat: createMockAds funtion

--- js/utils.js
+++ js/utils.js
@@ -1,40 +1,40 @@
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min
+  return Math.random() * (max - min) + min;
 }
 
 function getRandomFloat(min, max, digits = 0) {
-  return getRandomNumber(min, max).toFixed(digits)
+  return getRandomNumber(min, max).toFixed(digits);
 }
 
 function getRandomInteger(min, max) {
-  return getRandomFloat(min, max)
+  return getRandomFloat(min, max);
 }
 
 function checkSequence(min, max) {
-  const errors = []
+  const errors = [];
 
-  0 > min && errors.push('0 greater than min')
-  0 > max === -1 && errors.push('0 greater than max')
-  min > max && errors.push('min greater than max')
-  min === max && errors.push('min equals max')
+  0 > min && errors.push('0 greater than min');
+  0 > max === -1 && errors.push('0 greater than max');
+  min > max && errors.push('min greater than max');
+  min === max && errors.push('min equals max');
 
   if (errors.length) {
-    throw new Error(errors.join(', '))
+    throw new Error(errors.join(', '));
   }
 
-  return true
+  return true;
 }
 
 function getRandomPositiveFloat(min, max, digits) {
   if (checkSequence(min, max)) {
-    return getRandomFloat(min, max, digits)
+    return getRandomFloat(min, max, digits);
   }
 }
 
 function getRandomPositiveInteger(min, max) {
   if (checkSequence(min, max)) {
-    return getRandomInteger(min, max)
+    return getRandomInteger(min, max);
   }
 }
 
-export { getRandomPositiveFloat, getRandomPositiveInteger }
+export { getRandomPositiveFloat, getRandomPositiveInteger };
